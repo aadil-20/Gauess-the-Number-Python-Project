835701169daa43747d612cae5e70f1f9b9c48335
aadil-20 <70330940+aadil-20@users.noreply.github.com>
2021-05-01 14:36:52 +0530

    Add files via upload

---
+++ Project 1-Guess the Number.py
@@ -0,0 +1,19 @@
+import random
+true_number = random.randint(1, 49)
+
+guess_number = int(input("Enter your guess between 1 to 49:- "))
+
+
+
+while True:
+    if guess_number == true_number:
+        print("YOU ARE RIGHT! GOOD  JOB")
+        break
+
+    elif guess_number < true_number:
+        print("YOUR GUESS IS LOW TRY AGAIN")
+        guess_number = int(input("Enter Your Guess between 1 to 49:- "))
+
+    elif guess_number > true_number:
+        print("YOUR GUESS IS HIGH TRY AGAIN")
+        guess_number = int(input("Enter an integer from 1 and 49:- "))
